2ffb78994dfcd3b04b6ad3e345db68541d59d941
mayank sareen <mayanksareen63@gmail.com>
2018-03-06 22:20:39 +0530

    Added core functions

--- phpJwt.php
+++ phpJwt.php
@@ -6,7 +6,8 @@
 	class phpJwt
 	{
 		private $algo_type;
-		private $payload_data;
+		private $payload_data=array();
+		private $header_data;
 		const ALGO_TYPE_HS256 = 0;
 		const ALGO_TYPE_HS384 = 1;
 		const ALGO_TYPE_HS512 = 2;
@@ -23,17 +24,48 @@
 		*/
 		function createHeader($algo_type,$type="JWT",$cty=null){
 			if($cty==null){
-				$header_data = ["type"=>"JWT","alg"=>"'".$this->supported_algos[$algo_type]."'"];	
+				$this->header_data = ["type"=>"JWT","alg"=>"'".$this->supported_algos[$algo_type]."'"];	
 			}else{
-				$header_data = ["type"=>"JWT","alg"=>"'".$this->supported_algos[$algo_type]."'","cty"=>"'".$cty."'"];
+				$this->header_data = ["type"=>"JWT","alg"=>"'".$this->supported_algos[$algo_type]."'","cty"=>"'".$cty."'"];
 			}
 			$header_data = phpJwt::base64_generator(phpJwt::json_generator($header_data));
 			var_dump($header_data);
 		}
 
 		//This function creates generic standard payload
-		function createPayload(){
-			echo "Payload is created Here";
+		/**
+		*@param String $iss: Defines issuer string.
+		*@param String $sub: Defines the princi[pal of the subject JWT.
+		*@param String $aud: Defines the recepient that the JWT is intended for.
+		*@param int $exp:	Defines the expiration time for the JWT
+		*@param int $nbf: 	Defines the time before the JWT must not be accepted.
+		*@param int $iat:	Defines the time at which the JWT is issued at.
+		*@param int $jti:	Defines the unique identifier for the JWT.
+		*/
+		function createPayload($iss=null,$sub=null,$aud=null,$exp=null,$nbf=null,$iat=null,$jti=null){
+				if($this->$iss!=null)
+				{
+					$this->payload_data["iss"] = $iss;
+				}
+				if($this->$sub!=null){
+					$this->payload_data["sub"] = $sub;
+				}
+				if($this->$aud!=null){
+					$this->payload_data["aud"] = $aud;
+				}
+				if($this->exp!=null){
+					$this->payload_data["exp"] = $exp;
+				}
+				if($this->nbf!=null){
+					$this->payload_data["nbf"] = $nbf;
+				}
+				if($this->iat!=null){
+					$this->payload_data["iat"] = $iat;
+				}
+				if($thsi->jti!-null){
+					$this->payload_data["jti"] = $jti;
+				}
+				
 		}
 
 
@@ -42,6 +74,8 @@
 		function createExtraPayload(){
 			
 		}	
+
+
 
 		//Converts array data into json data
 		//Currently uses json_encode() generic function
